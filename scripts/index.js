4252756ba7a01ce7811fc56562747fe1852c3d12
JackPumpkinHead <kostyu444enko.igor@gmail.com>
2022-09-23 23:39:31 +0300

    Create modal

--- scripts/index.js
+++ scripts/index.js
@@ -22,5 +22,39 @@ new Swiper('.goods__block', {
     navigation: {
         prevEl: '.goods__arrow_prev',
         nextEl: '.goods__arrow_next'
+    },
+    preventClicks: true,
+    ally: false,
+});
+
+const productMore = document.querySelectorAll('.product__more');
+
+
+const modal = document.querySelector('.modal');
+const formPlaceholder = document.querySelectorAll('.form__placeholder');
+const formInput = document.querySelectorAll('.form__input')
+
+productMore.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        modal.classList.add('modal_open');
+    })
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.classList.remove('modal_open');
     }
+});
+
+
+formInput.forEach((input, i) => {
+    input.addEventListener('focus', () => {
+        formPlaceholder[i].classList.add('form__placeholder_active');
+    })
+
+    input.addEventListener('blur', () => {
+        if (input.value === ''){
+            formPlaceholder[i].classList.remove('form__placeholder_active');
+        }
+    })
 })
